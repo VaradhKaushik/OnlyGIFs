6a6b53867c5f6f215d4b4a6caea9eddb0020e938
Aryan Methil <aryanmethil@gmail.com>
2020-10-26 17:59:51 +0530

    configured home page files

--- Frontend files/Home_Page.php
+++ Frontend files/Home_Page.php
@@ -1,3 +1,8 @@
+<?php
+error_reporting(E_ALL);
+session_start();
+$x=7;
+?>
 <!DOCTYPE html>
 <html style="width: 100vw;height: 100vh;">
 
@@ -38,6 +43,77 @@
 </head>
 
 <body style="background: #303a44;height: 1080px;color: rgb(255,255,255);width: 100vw;">
+
+
+
+    <?php
+    
+    function OpenCon()
+      {
+       $dbhost = "localhost";
+       $dbuser = "root";
+       $dbpass = "1234";
+       $db = "OnlyGif";
+       $conn = mysqli_connect($dbhost, $dbuser, $dbpass,$db);
+       
+       return $conn;
+     }
+
+      function CloseCon($conn)
+      {
+       $conn -> close();
+      }
+
+      $conn = OpenCon();
+      if($conn === false){
+        die("ERROR: Could not connect. " . $conn->connect_error);
+      } 
+      ?>
+
+      
+      <?php
+      function getImageURL($conn,$num){
+        $sql2="SELECT * FROM images";
+        $result_2=$conn->query($sql2);
+        $rows=$result_2->num_rows;
+
+        $num=$num%$rows;
+        if($num==0)
+          $num=$rows;
+        $sql3 = "SELECT * FROM images where imgid='$num'";  
+        $result = $conn->query($sql3);
+
+         
+
+        if ($result->num_rows> 0) {
+          // output data of each row
+          while($row = $result->fetch_assoc()) {
+            $imgsrc=$row["image"];
+            $image_base64=explode(';base64,',$imgsrc)[1];
+            $image_ur="data:image/gif;base64,$image_base64";
+          } 
+        }
+        return $image_ur;
+      }
+      ?>
+
+
+    <script type="text/javascript">
+
+    function refresh1(id_number){
+      <?php 
+      global $x;
+      ?>;
+      document.getElementById(String(id_number)).src = "<?php echo getImageURL($conn,$x); ?>";
+      <?php
+      ++$x;
+      ?>
+    }
+    </script>
+
+
+
+
     <nav class="navbar navbar-light navbar-expand-md fixed-top" style="color: #ffffff;background: #322f2f;height: 50px;width: 100vw;">
         <div class="container-fluid"><a class="navbar-brand" href="#" style="color: #ffffff;font-size: 31px;margin-top: -15px;">OnlyGIFs</a>
             <ul class="nav navbar-nav">
@@ -48,37 +124,37 @@
         </div>
     </nav>
     <div class="card-group" style="margin-top: 50px;height: 270px;background: #595757;width: 100vw;border-width: 8px;border-color: #1f2021;">
-        <div class="card" style="width: 640px;"><img class="card-img-top w-100 d-block" style="height: 320px;border-style: none;" src="assets/img/patrick_scream.gif">
+        <div class="card" style="width: 640px;"><img id="1" class="card-img-top w-100 d-block" src="<?php echo getImageURL($conn,1); ?>" style="height: 320px;border-style: none;">
             <div class="card-body" style="background: #475d62;width: 100vw;border: 1px solid #222222 ;">
                 <h4 class="card-title" style="color: #ffffff;font-size: 20px;text-align: left;">Username</h4>
-                <p class="card-text" style="color: rgb(255,255,255);font-size: 16px;">caption idhar aayega</p><i class="far fa-heart" data-bs-hover-animate="rubberBand" style="margin-left: 518px;"></i><i class="fa fa-refresh" data-bs-hover-animate="flash" style="margin-left: 15px;"></i></div>
+                <p class="card-text" style="color: rgb(255,255,255);font-size: 16px;">caption idhar aayega</p><i class="far fa-heart" data-bs-hover-animate="rubberBand" style="margin-left: 518px;"></i><br><button class="btn btn-primary" type="button" style="text-align: center;" onclick="refresh1(1)">Button</button></div>
         </div>
-        <div class="card" style="width: 640px;"><img class="card-img-top w-100 d-block" style="height: 320px;border-style: none;" src="assets/img/batman_interesting.gif">
+        <div class="card" style="width: 640px;"><img id="2" class="card-img-top w-100 d-block" src="<?php echo getImageURL($conn,2); ?>" style="height: 320px;border-style: none;">
             <div class="card-body" style="background: #475d62;border: 1px solid #222222 ;">
                 <h4 class="card-title" style="color: #ffffff;font-size: 20px;text-align: left;">Username</h4>
-                <p class="card-text" style="color: rgb(255,255,255);font-size: 16px;">caption idhar aayega<br></p><i class="far fa-heart" data-bs-hover-animate="rubberBand" style="margin-left: 518px;"></i><i class="fa fa-refresh" data-bs-hover-animate="flash" style="margin-left: 15px;"></i></div>
+                <p class="card-text" style="color: rgb(255,255,255);font-size: 16px;">caption idhar aayega<br></p><i class="far fa-heart" data-bs-hover-animate="rubberBand" style="margin-left: 518px;"></i><br><button class="btn btn-primary" type="button" style="text-align: center;" onclick="refresh1(2)">Button</button></div>
         </div>
-        <div class="card" style="width: 640px;"><img class="card-img-top w-100 d-block" style="height: 320px;border-style: none;" src="assets/img/barney_stroking.gif">
+        <div class="card" style="width: 640px;"><img id="3" class="card-img-top w-100 d-block" src="<?php echo getImageURL($conn,3); ?>" style="height: 320px;border-style: none;">
             <div class="card-body" style="background: #475d62;border: 1px solid #222222 ;">
                 <h4 class="card-title" style="color: #ffffff;font-size: 20px;text-align: left;">Username</h4>
-                <p class="card-text" style="color: rgb(255,255,255);font-size: 16px;">caption idhar aayega<br></p><i class="far fa-heart" data-bs-hover-animate="rubberBand" style="margin-left: 518px;"></i><i class="fa fa-refresh" data-bs-hover-animate="flash" style="margin-left: 15px;"></i></div>
+                <p class="card-text" style="color: rgb(255,255,255);font-size: 16px;">caption idhar aayega<br></p><i class="far fa-heart" data-bs-hover-animate="rubberBand" style="margin-left: 518px;"></i><br><button class="btn btn-primary" type="button" style="text-align: center;" onclick="refresh1(3)">Button</button></div>
         </div>
     </div>
     <div class="card-group" style="margin-top: 200px;height: 270px;background: #595757;width: 100vw;border-width: 8px;border-color: #1f2021;">
-        <div class="card" style="width: 640px;"><img class="card-img-top w-100 d-block" style="height: 320px;border-style: none;" src="assets/img/squidward_throwing_it_back.gif">
+        <div class="card" style="width: 640px;"><img id="4" class="card-img-top w-100 d-block" src="<?php echo getImageURL($conn,4); ?>" style="height: 320px;border-style: none;">
             <div class="card-body" style="background: #475d62;width: 100vw;border: 1px solid #222222 ;">
                 <h4 class="card-title" style="color: #ffffff;font-size: 20px;text-align: left;">Username</h4>
-                <p class="card-text" style="color: rgb(255,255,255);font-size: 16px;">caption idhar aayega</p><i class="far fa-heart" data-bs-hover-animate="rubberBand" style="margin-left: 518px;"></i><i class="fa fa-refresh" data-bs-hover-animate="flash" style="margin-left: 15px;"></i></div>
+                <p class="card-text" style="color: rgb(255,255,255);font-size: 16px;">caption idhar aayega</p><i class="far fa-heart" data-bs-hover-animate="rubberBand" style="margin-left: 518px;"></i><br><button class="btn btn-primary" type="button" style="text-align: center;" onclick="refresh1(4)">Button</button></div>
         </div>
-        <div class="card" style="width: 640px;"><img class="card-img-top w-100 d-block" style="height: 320px;border-style: none;" src="assets/img/pooh_dance.gif">
+        <div class="card" style="width: 640px;"><img id="5" class="card-img-top w-100 d-block" src="<?php echo getImageURL($conn,5); ?>" style="height: 320px;border-style: none;">
             <div class="card-body" style="background: #475d62;border: 1px solid #222222 ;">
                 <h4 class="card-title" style="color: #ffffff;font-size: 20px;text-align: left;">Username</h4>
-                <p class="card-text" style="color: rgb(255,255,255);font-size: 16px;">caption idhar aayega<br></p><i class="far fa-heart" data-bs-hover-animate="rubberBand" style="margin-left: 518px;"></i><i class="fa fa-refresh" data-bs-hover-animate="flash" style="margin-left: 15px;"></i></div>
+                <p class="card-text" style="color: rgb(255,255,255);font-size: 16px;">caption idhar aayega<br></p><i class="far fa-heart" data-bs-hover-animate="rubberBand" style="margin-left: 518px;"></i><br><button class="btn btn-primary" type="button" style="text-align: center;" onclick="refresh1(5)">Button</button></div>
         </div>
-        <div class="card" style="width: 640px;"><img class="card-img-top w-100 d-block" style="height: 320px;border-style: none;" src="assets/img/unicorn_wishing.gif">
+        <div class="card" style="width: 640px;"><img id="6" class="card-img-top w-100 d-block" src="<?php echo getImageURL($conn,6); ?>" style="height: 320px;border-style: none;">
             <div class="card-body" style="background: #475d62;border: 1px solid #222222 ;">
                 <h4 class="card-title" style="color: #ffffff;font-size: 20px;text-align: left;">Username</h4>
-                <p class="card-text" style="color: rgb(255,255,255);font-size: 16px;">caption idhar aayega<br></p><i class="far fa-heart" data-bs-hover-animate="rubberBand" style="margin-left: 518px;"></i><i class="fa fa-refresh" data-bs-hover-animate="flash" style="margin-left: 15px;"></i></div>
+                <p class="card-text" style="color: rgb(255,255,255);font-size: 16px;">caption idhar aayega<br></p><i class="far fa-heart" data-bs-hover-animate="rubberBand" style="margin-left: 518px;"></i><br><button class="btn btn-primary" type="button" style="text-align: center;" name="refresh" onclick="refresh1(6)">Button</button></div>
         </div>
     </div>
     <script src="assets/js/jquery.min.js"></script>
