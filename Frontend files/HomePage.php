0c0723dcada0cdc6461c2d81828663b02f178ea7
Aryan Methil <aryanmethil@gmail.com>
2020-10-28 12:49:40 +0530

    Updated home page to retrieve number of likes and image caption from db and updated the refresh function accordingly. Added query for creating liked images table. onclick of like button not working properly

--- Frontend files/HomePage.php
+++ Frontend files/HomePage.php
@@ -8,7 +8,7 @@ $x=7;
 <html>
 
 <head>
-   <meta charset="utf-8">
+    <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
     <title>OnlyGifs</title>
     <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
@@ -69,7 +69,17 @@ $x=7;
       if($conn === false){
         die("ERROR: Could not connect. " . $conn->connect_error);
       } 
-      ?>
+
+      $sql_liked = "CREATE TABLE IF NOT EXISTS LikedImages(
+        uid VARCHAR(200) NOT NULL,
+        imgid INT NOT NULL,
+        PRIMARY KEY(uid,imgid)
+      )";
+
+      if( $conn->query($sql_liked) === false ){
+        die("ERROR: Could not able to execute $sql. " . $conn->error);
+      }
+?>
 
       
       <?php
@@ -85,16 +95,23 @@ $x=7;
         $result = $conn->query($sql3);
 
          
-
+        $arr=array();
         if ($result->num_rows> 0) {
           // output data of each row
           while($row = $result->fetch_assoc()) {
             $imgsrc=$row["image"];
             $image_base64=explode(';base64,',$imgsrc)[1];
             $image_ur="data:image/gif;base64,$image_base64";
+
+            $caption_text=$row["caption"];
+
+            $likes=$row["likes"];
+
+            array_push($arr, $image_ur, $caption_text, $likes);
+
           } 
         }
-        return $image_ur;
+        return $arr;
       }
       ?>
 
@@ -105,88 +122,119 @@ $x=7;
       <?php 
       global $x;
       ?>;
-      document.getElementById(String(id_number)).src = "<?php echo getImageURL($conn,$x); ?>";
+      document.getElementById(String(id_number)).src = "<?php echo getImageURL($conn,$x)[0]; ?>";
+      var caption_id=String(id_number)+"_caption";
+      document.getElementById(caption_id).innerHTML="<?php echo getImageURL($conn,$x)[1]; ?>";
+      var likes_id=String(id_number)+"_likes";
+      document.getElementById(likes_id).innerHTML="<?php echo getImageURL($conn,$x)[2]; ?>"+" likes";
+      var hidden_imgid=String(id_number)+"_IMGID";
+      document.getElementById(hidden_imgid).innerHTML="<?php echo $x; ?>";
       <?php
       ++$x;
       ?>
+    }
+
+    function updateLikes(id_number){
+        var card_no=String(id_number)+"_IMGID";
+        <?php $img_id = "<script>document.getElementById(card_no).innerHTML;</script>"; 
+        $sql4 = "SELECT * FROM likedimages where imgid='$img_id' AND uid='1'";  
+        $result = $conn->query($sql4);
+        if ($result->num_rows> 0) {
+
+            $sql5="DELETE FROM likedimages where imgid='$img_id' AND uid='1'";
+            $delete_result = $conn->query($sql5);
+        }
+        else{
+            $sql6="INSERT INTO likedimages SET imgid='$img_id', uid='1'";
+            $insert_result=$conn ->query($sql6);
+        }
+        ?>
+
     }
     </script>
 
     <nav class="navbar navbar-light navbar-expand-md fixed-top" style="color: #ffffff;background: #322f2f;height: 50px;width: 100vw;">
         <div class="container-fluid"><a class="navbar-brand" href="#" style="color: #ffffff;font-size: 31px;margin-top: -15px;">OnlyGIFs</a>
             <ul class="nav navbar-nav">
-                <li class="nav-item" style="margin-left: -129px;"><a class="nav-link active" href="Profile.html" style="color: #ffffff;margin-left: 935px;font-size: 19px;margin-top: -15px;">Profile</a></li>
+                <li class="nav-item"><a class="nav-link active" href="Profile.php" style="color: #ffffff;margin-left: 935px;font-size: 19px;margin-top: -15px;">Profile</a></li>
                 <li class="nav-item"><a class="nav-link" href="LikedImages.html" style="color: #ffffff;font-size: 19px;margin-top: -15px;">Liked Images</a></li>
-                <li class="nav-item"><a class="nav-link" href="AddImage.html" style="color: #ffffff;font-size: 19px;margin-top: -15px;margin-left: -678px;width: 198px;background: url(&quot;assets/img/upload_me_harder_daddy.png&quot;) no-repeat;background-size: contain;">&nbsp; &nbsp; &nbsp; &nbsp; Upload GIF</a></li>
-                <li
-                    class="nav-item"><a class="nav-link" href="Start.html" style="color: #ffffff;font-size: 19px;margin-top: -15px;">Logout</a></li>
+                <li class="nav-item"><a class="nav-link" href="Start.html" style="color: #ffffff;font-size: 19px;margin-top: -15px;">Logout</a></li>
             </ul>
         </div>
     </nav>
 
     <div class="card-group" style="margin-top: 50px;height: 270px;background: #595757;width: 100vw;border-width: 8px;border-color: #1f2021;">
-        <div class="card" style="width: 640px;"><img class="card-img-top w-100 d-block" id="1" style="height: 320px;border-style: none;" src="<?php echo getImageURL($conn,1); ?>" >
+        <div class="card" style="width: 640px;"><img class="card-img-top w-100 d-block" id="1" style="height: 320px;border-style: none;" src="<?php echo getImageURL($conn,1)[0]; ?>" >
             <div class="card-body" style="background: #475d62;width: 100vw;border: 1px solid #222222;">
-                <h4 class="card-title" style="color: #ffffff;font-size: 20px;text-align: left;">Username<i class="far fa-heart" data-bs-hover-animate="rubberBand" style="margin-left: 125px;"></i>
+                <h4 class="card-title" style="color: #ffffff;font-size: 20px;text-align: left;">Username<i class="far fa-heart" data-bs-hover-animate="rubberBand" style="margin-left: 125px;"></i><span style="margin-left: 20px" id="1_likes" onclick="updateLikes(1)"><?php echo getImageURL($conn,1)[2]; ?> likes</span>
 
                     <button class="btn btn-primary" type="button" style="text-align: center; margin-left: 66px;" onclick="refresh1(1)">
 
                     <i class="fa fa-refresh" data-bs-hover-animate="flash" style="align: center; "></i></button>
 
                 </h4>
-                <p class="card-text" style="color: rgb(255,255,255);font-size: 16px;">caption idhar aayega</p>
+                <p class="card-text" style="color: rgb(255,255,255);font-size: 16px;" id="1_caption"><?php echo getImageURL($conn,1)[1]; ?> </p>
+                <p hidden id="1_IMGID">1</p>
             </div>
         </div>
 
-        <div class="card" style="width: 640px;"><img class="card-img-top w-100 d-block" style="height: 320px;border-style: none;" id="2" src="<?php echo getImageURL($conn,2); ?>">
+        <div class="card" style="width: 640px;"><img class="card-img-top w-100 d-block" style="height: 320px;border-style: none;" id="2" src="<?php echo getImageURL($conn,2)[0]; ?>">
             <div class="card-body" style="background: #475d62;border: 1px solid #222222;">
-                <h4 class="card-title" style="color: #ffffff;font-size: 20px;text-align: left;">Username<i class="far fa-heart" data-bs-hover-animate="rubberBand" style="margin-left: 125px;"></i>
+                <h4 class="card-title" style="color: #ffffff;font-size: 20px;text-align: left;">Username
+                    <i class="far fa-heart" data-bs-hover-animate="rubberBand" style="margin-left: 125px;"></i>
+                    <span style="margin-left: 20px" id="2_likes"><?php echo getImageURL($conn,2)[2]; ?> likes</span>
+
                     <button class="btn btn-primary" type="button" style="text-align: center; margin-left: 66px;" onclick="refresh1(2)">
                     <i class="fa fa-refresh" data-bs-hover-animate="flash" style="align: center; "></i></button>
                 </h4>
-                <p class="card-text" style="color: rgb(255,255,255);font-size: 16px;">caption idhar aayega<br></p>
+                <p class="card-text" style="color: rgb(255,255,255);font-size: 16px;" id="2_caption"><?php echo getImageURL($conn,2)[1]; ?></p>
+                <p hidden id="2_IMGID">2</p>
             </div>
         </div>
 
-        <div class="card" style="width: 640px;"><img class="card-img-top w-100 d-block" style="height: 320px;border-style: none;" id="3" src="<?php echo getImageURL($conn,3); ?>">
+        <div class="card" style="width: 640px;"><img class="card-img-top w-100 d-block" style="height: 320px;border-style: none;" id="3" src="<?php echo getImageURL($conn,3)[0]; ?>">
             <div class="card-body" style="background: #475d62;border: 1px solid #222222;">
-                <h4 class="card-title" style="color: #ffffff;font-size: 20px;text-align: left;">Username<i class="far fa-heart" data-bs-hover-animate="rubberBand" style="margin-left: 125px;"></i>
+                <h4 class="card-title" style="color: #ffffff;font-size: 20px;text-align: left;">Username<i class="far fa-heart" data-bs-hover-animate="rubberBand" style="margin-left: 125px;"></i><span style="margin-left: 20px" id="3_likes"><?php echo getImageURL($conn,3)[2]; ?> likes</span>
                     <button class="btn btn-primary" type="button" style="text-align: center; margin-left: 66px;" onclick="refresh1(3)">
                     <i class="fa fa-refresh" data-bs-hover-animate="flash" style="align: center; "></i></button>
                 </h4>
-                <p class="card-text" style="color: rgb(255,255,255);font-size: 16px;">caption idhar aayega<br></p>
+                <p class="card-text" style="color: rgb(255,255,255);font-size: 16px;" id="3_caption"><?php echo getImageURL($conn,3)[1]; ?><br></p>
+                <p hidden id="3_IMGID">3</p>
             </div>
         </div>
     </div>
 
     <div class="card-group" style="margin-top: 200px;height: 270px;background: #595757;width: 100vw;border-width: 8px;border-color: #1f2021;">
-        <div class="card" style="width: 640px;"><img class="card-img-top w-100 d-block" style="height: 320px;border-style: none;" id="4" src="<?php echo getImageURL($conn,4); ?>">
+        <div class="card" style="width: 640px;"><img class="card-img-top w-100 d-block" style="height: 320px;border-style: none;" id="4" src="<?php echo getImageURL($conn,4)[0]; ?>">
             <div class="card-body" style="background: #475d62;width: 100vw;border: 1px solid #222222;">
-                <h4 class="card-title" style="color: #ffffff;font-size: 20px;text-align: left;">Username<i class="far fa-heart" data-bs-hover-animate="rubberBand" style="margin-left: 125px;"></i>
+                <h4 class="card-title" style="color: #ffffff;font-size: 20px;text-align: left;">Username<i class="far fa-heart" data-bs-hover-animate="rubberBand" style="margin-left: 125px;"></i><span style="margin-left: 20px" id="4_likes"><?php echo getImageURL($conn,4)[2]; ?> likes</span>
                     <button class="btn btn-primary" type="button" style="text-align: center; margin-left: 66px;" onclick="refresh1(4)">
                     <i class="fa fa-refresh" data-bs-hover-animate="flash" style="align: center; "></i></button>
                 </h4>
-                <p class="card-text" style="color: rgb(255,255,255);font-size: 16px;">caption idhar aayega</p>
+                <p class="card-text" style="color: rgb(255,255,255);font-size: 16px;" id="4_caption"><?php echo getImageURL($conn,4)[1]; ?></p>
+                <p hidden id="4_IMGID">4</p>
             </div>
         </div>
 
-        <div class="card" style="width: 640px;"><img class="card-img-top w-100 d-block" style="height: 320px;border-style: none;" id="5" src="<?php echo getImageURL($conn,5); ?>">
+        <div class="card" style="width: 640px;"><img class="card-img-top w-100 d-block" style="height: 320px;border-style: none;" id="5" src="<?php echo getImageURL($conn,5)[0]; ?>">
             <div class="card-body" style="background: #475d62;border: 1px solid #222222;">
-                <h4 class="card-title" style="color: #ffffff;font-size: 20px;text-align: left;">Username<i class="far fa-heart" data-bs-hover-animate="rubberBand" style="margin-left: 125px;"></i>
+                <h4 class="card-title" style="color: #ffffff;font-size: 20px;text-align: left;">Username<i class="far fa-heart" data-bs-hover-animate="rubberBand" style="margin-left: 125px;"></i><span style="margin-left: 20px" id="5_likes"><?php echo getImageURL($conn,5)[2]; ?> likes</span>
                     <button class="btn btn-primary" type="button" style="text-align: center; margin-left: 66px;" onclick="refresh1(5)">
                     <i class="fa fa-refresh" data-bs-hover-animate="flash" style="align: center; "></i></button>
                 </h4>
-                <p class="card-text" style="color: rgb(255,255,255);font-size: 16px;">caption idhar aayega<br></p>
+                <p class="card-text" style="color: rgb(255,255,255);font-size: 16px;" id="5_caption"><?php echo getImageURL($conn,5)[1]; ?><br></p>
+                <p hidden id="5_IMGID">5</p>
             </div>
         </div>
 
-        <div class="card" style="width: 640px;"><img class="card-img-top w-100 d-block" style="height: 320px;border-style: none;" id="6" src="<?php echo getImageURL($conn,6); ?>">
+        <div class="card" style="width: 640px;"><img class="card-img-top w-100 d-block" style="height: 320px;border-style: none;" id="6" src="<?php echo getImageURL($conn,6)[0]; ?>">
             <div class="card-body" style="background: #475d62;border: 1px solid #222222;">
-                <h4 class="card-title" style="color: #ffffff;font-size: 20px;text-align: left;">Username<i class="far fa-heart" data-bs-hover-animate="rubberBand" style="margin-left: 125px;"></i>
+                <h4 class="card-title" style="color: #ffffff;font-size: 20px;text-align: left;">Username<i class="far fa-heart" data-bs-hover-animate="rubberBand" style="margin-left: 125px;"></i><span style="margin-left: 20px" id="6_likes"><?php echo getImageURL($conn,6)[2]; ?> likes</span>
                     <button class="btn btn-primary" type="button" style="text-align: center; margin-left: 66px;" onclick="refresh1(6)">
                     <i class="fa fa-refresh" data-bs-hover-animate="flash" style="align: center; "></i></button>
                 </h4>
-                <p class="card-text" style="color: rgb(255,255,255);font-size: 16px;">caption idhar aayega<br></p>
+                <p class="card-text" style="color: rgb(255,255,255);font-size: 16px;" id="6_caption"><?php echo getImageURL($conn,6)[1]; ?><br></p>
+                <p hidden id="6_IMGID">6</p>
             </div>
         </div>
     </div>
